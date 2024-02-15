37fb3816a319f9ca4e747698677d1945ea059ecf
Gaibryol <gaibryol@gmail.com>
2024-02-15 13:19:26 -0700

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,89 @@
+﻿using BepInEx.Configuration;
+using CSync.Lib;
+using CSync.Util;
+using System;
+using System.Runtime.Serialization;
+using HarmonyLib;
+using Unity.Collections;
+using Unity.Netcode;
+
+namespace SuperiorSelling
+{
+	[DataContract]
+	public class Config : SyncedInstance<Config>
+	{
+		[DataMember] public SyncedEntry<int> MaximumItems { get; private set; }
+
+		public Config(ConfigFile cfg)
+		{
+			InitInstance(this);
+
+			MaximumItems = cfg.BindSyncedEntry(
+				"General",
+				"MaximumItemsToSell",
+				-1,
+				"Maximum amount of items that can be sold at once (Infinite = -1, Vanilla Lethal Company = 12)"
+			);
+		}
+
+		internal static void RequestSync()
+		{
+			if (!IsClient) return;
+
+			using FastBufferWriter stream = new(IntSize, Allocator.Temp);
+
+			// Method `OnRequestSync` will then get called on host.
+			stream.SendMessage($"{PluginInfo.PLUGIN_GUID}_OnRequestConfigSync");
+		}
+
+		internal static void OnRequestSync(ulong clientId, FastBufferReader _)
+		{
+			if (!IsHost) return;
+
+			byte[] array = SerializeToBytes(Instance);
+			int value = array.Length;
+
+			using FastBufferWriter stream = new(value + IntSize, Allocator.Temp);
+
+			try
+			{
+				stream.WriteValueSafe(in value, default);
+				stream.WriteBytesSafe(array);
+
+				stream.SendMessage($"{PluginInfo.PLUGIN_GUID}_OnReceiveConfigSync", clientId);
+			}
+			catch (Exception e)
+			{
+				Plugin.Logger.LogError($"Error occurred syncing config with client: {clientId}\n{e}");
+			}
+		}
+
+		internal static void OnReceiveSync(ulong _, FastBufferReader reader)
+		{
+			if (!reader.TryBeginRead(IntSize))
+			{
+				Plugin.Logger.LogError("Config sync error: Could not begin reading buffer.");
+				return;
+			}
+
+			reader.ReadValueSafe(out int val, default);
+			if (!reader.TryBeginRead(val))
+			{
+				Plugin.Logger.LogError("Config sync error: Host could not sync.");
+				return;
+			}
+
+			byte[] data = new byte[val];
+			reader.ReadBytesSafe(ref data, val);
+
+			try
+			{
+				SyncInstance(data);
+			}
+			catch (Exception e)
+			{
+				Plugin.Logger.LogError($"Error syncing config instance!\n{e}");
+			}
+		}
+	}
+}
---
+++ Plugin.cs
@@ -0,0 +1,119 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using CSync.Lib;
+using GameNetcodeStuff;
+using HarmonyLib;
+using SuperiorSelling.Patches;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace SuperiorSelling
+{
+	[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+	[BepInDependency("io.github.CSync")]
+	public class Plugin : BaseUnityPlugin
+	{
+		private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+		public static Config Config { get; private set; }
+
+		public static Plugin Instance { get; internal set; }
+
+		public static new ManualLogSource Logger { get; private set; }
+
+		private void Awake()
+		{
+			if (Instance == null)
+			{
+				Instance = this;
+			}
+
+			Logger = base.Logger;
+
+			Config = new(base.Config);
+
+			harmony.PatchAll(typeof(DepositItemsPatch));
+			harmony.PatchAll(typeof(PlayerControllerBPatch));
+			harmony.PatchAll(typeof(NetworkManagerPatch));
+			harmony.PatchAll(typeof(Config));
+			//harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+			// Plugin startup logic
+			Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} is loaded!");
+		}
+	}
+
+	public static class PluginInfo
+	{
+		public const string PLUGIN_GUID = "SuperiorSelling";
+		public const string PLUGIN_NAME = "SuperiorSelling";
+		public const string PLUGIN_VERSION = "1.0.0";
+	}
+}
+
+namespace SuperiorSelling.Patches
+{
+	[HarmonyPatch(typeof(DepositItemsDesk))]
+	internal class DepositItemsPatch
+	{
+		[HarmonyTranspiler]
+		[HarmonyPatch("PlaceItemOnCounter")]
+		static IEnumerable<CodeInstruction> PlaceItemOnCounterTranspiler(IEnumerable<CodeInstruction> instructions)
+		{
+			// Iterate through CodeInstructions
+			List<CodeInstruction> newInstructions = instructions.ToList();
+			for(int i = 0; i < newInstructions.Count - 1; i++)
+			{
+				// Find line of code we need to replace
+                if (newInstructions[i].opcode != OpCodes.Ldc_I4_S)
+				{
+					continue;
+				}
+
+				// Replace comparison
+				newInstructions[i] = new CodeInstruction(OpCodes.Ldc_I4_S, (Config.Instance.MaximumItems.Value == -1 ? int.MaxValue : Config.Instance.MaximumItems.Value));
+            }
+			return newInstructions;
+		}
+	}
+
+	[HarmonyPatch(typeof(PlayerControllerB))]
+	internal class PlayerControllerBPatch
+	{
+		[HarmonyPostfix]
+		[HarmonyPatch("ConnectClientToPlayerObject")]
+		public static void InitializeLocalPlayer()
+		{
+			if (Config.IsHost)
+			{
+				Config.MessageManager.RegisterNamedMessageHandler($"{PluginInfo.PLUGIN_GUID}_OnRequestConfigSync", Config.OnRequestSync);
+				Config.Synced = true;
+
+				Plugin.Logger.LogInfo("Initialize Is Host");
+				return;
+			}
+
+			Plugin.Logger.LogInfo("Initialize Client");
+			Config.Synced = false;
+			Config.MessageManager.RegisterNamedMessageHandler($"{PluginInfo.PLUGIN_GUID}_OnReceiveConfigSync", Config.OnReceiveSync);
+			Config.RequestSync();
+		}
+	}
+
+	[HarmonyPatch(typeof(GameNetworkManager))]
+	internal class NetworkManagerPatch
+	{
+		[HarmonyPostfix]
+		[HarmonyPatch("StartDisconnect")]
+		public static void PlayerLeave()
+		{
+			Config.RevertSync();
+		}
+	}
+}
